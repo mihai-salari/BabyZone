e0f1942bd7592310ef44c7e1b2e62261a9e77270
Sam <1032479745@qq.com>
2016-10-18 18:02:32 +0800

    1.0.7

--- EnjoyLove/CommonMethod/Database/CountryCode.m
+++ EnjoyLove/CommonMethod/Database/CountryCode.m
@@ -18,3 +18,42 @@
     
 }
 @end
+
+/*
+ + (Database *)shared{
+ static Database *sharedManager = nil;
+ static dispatch_once_t once;
+ dispatch_once(&once, ^{
+ 
+ sharedManager = [[self alloc] init];
+ [sharedManager createEditableCopyOfDatabaseIfNeeded];
+ });
+ return sharedManager;
+ }
+ 
+ - (void)createEditableCopyOfDatabaseIfNeeded {
+ 
+ NSString *writableDBPath = [self applicationDocumentsDirectoryFile];
+ 
+ if (sqlite3_open([writableDBPath UTF8String], &db) != SQLITE_OK) {
+ sqlite3_close(db);
+ NSAssert(NO,@"数据库打开失败。");
+ } else {
+ NSString *sqlPath = [[NSBundle mainBundle] pathForResource:@"ga_area" ofType:@"sql"];
+ char *err;
+ NSString *createSQL = [[NSString alloc] initWithContentsOfFile:sqlPath encoding:NSUTF8StringEncoding error:nil];
+ if (sqlite3_exec(db,[createSQL UTF8String],NULL,NULL,&err) != SQLITE_OK) {
+ sqlite3_close(db);
+ NSAssert1(NO, @"建表失败, %s", err);
+ }
+ sqlite3_close(db);
+ }
+ }
+ 
+ - (NSString *)applicationDocumentsDirectoryFile {
+ NSString *documentDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+ NSString *path = [documentDirectory stringByAppendingPathComponent:DBFILE_NAME];
+ 
+ return path;
+ }
+ */
